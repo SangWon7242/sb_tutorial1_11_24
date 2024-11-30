0034cbaba2ffd7b43439c14842fa08395dca61df
SangWon7242 <gotkdqja@gmail.com>
2024-11-30 10:38:44 +0900

    사람 추가 기능 구현

--- src/main/java/com/sbs/tutorial1/controller/HomeController.java
+++ src/main/java/com/sbs/tutorial1/controller/HomeController.java
@@ -1,9 +1,6 @@
 package com.sbs.tutorial1.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +14,11 @@ import java.util.*;
 // "그리고 해당 클래스는 컨트롤러야!"
 public class HomeController {
   int num;
+  List<Person> people;
 
   public HomeController() {
     num = -1;
+    people = new ArrayList<>();
   }
 
   @GetMapping("/home/main")
@@ -195,6 +194,22 @@ public class HomeController {
 
     return list;
   }
+
+  @GetMapping("/home/addPerson")
+  @ResponseBody
+  public String addPerson(String name, int age) {
+    Person p = new Person(name, age);
+    System.out.println("p " + p);
+    people.add(p);
+
+    return "%d번 사람이 추가되었습니다.".formatted(p.getId());
+  }
+
+  @GetMapping("/home/showPeople")
+  @ResponseBody
+  public List<Person> showPeople() {
+    return people;
+  }
 }
 
 class Article {
@@ -253,4 +268,22 @@ class Article2 {
   String content;
   String writerName;
   List<Integer> articleNo;
+}
+
+@AllArgsConstructor
+@Getter
+@ToString
+class Person {
+  private static int lastId;
+  private final int id;
+  private final String name;
+  private final int age;
+
+  static {
+    lastId = 0;
+  }
+
+  public Person(String name, int age) {
+    this(++lastId, name, age);
+  }
 }
